62e1453307e2f7f36bc0287e58d815a632fa6324
Himangshu Yadav <himangshuyadav8@gmail.com>
2024-11-01 17:09:49 +0530

    hsdgfhsda

--- test2.py
+++ test2.py
@@ -1,11 +1,4 @@
-from geopy.geocoders import Nominatim
-from geopy.distance import distance
-from math import ceil
-def calculate_distance(location1, location2):
-    geolocator=Nominatim(user_agent='distance_calculator')
-    location1=geolocator.geocode(location1)
-    location2=geolocator.geocode(location2)
-    return (distance((location1.latitude,location1.longitude),(location2.latitude,location2.longitude)).meters)/1000
-    
-    
-print(calculate_distance("delhi","shillong"))
+from datetime import datetime
+now = datetime.now()
+formatted_date = now.strftime('%Y-%m-%d')
+print(type(formatted_date))
